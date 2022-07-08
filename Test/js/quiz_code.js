751aab504869409c6f959a38c24af232c4b5e43f
Egor Yakovlev <yagorbit@mail.ru>
2022-07-08 23:39:13 +0300

    Moved Test-project to radio type

--- Test/js/quiz_code.js
+++ Test/js/quiz_code.js
@@ -1,11 +1,8 @@
-// вывод на экран самого вопроса при загрузке стр.
 window.onload = function () {
     let result = {};
-
-    // определяем шаг вопроса, на каком сейчас находимся
     let step = 0;
 
-    //показываем хотябы 1-й вопрос
+    //показываем 1-й вопрос
     function showQuestion(questionNumber) {
         //Вопрос
         document.querySelector('.question').innerHTML = quiz[step]['q'];
@@ -13,59 +10,73 @@ window.onload = function () {
         let answer = '';
         // перебираем массив со всеми вариантами ответов
         for (let key in quiz[step]['a']) {
-            answer += `<li data-v="${key}" class="answer-variant">${quiz[step]['a'][key]}</li>`;
+            answer += `<li>
+                <label>
+                    <input class="answer-variant" type="radio" name=${step} value="${key}">
+                    ${quiz[step]['a'][key]}
+                </label>
+            </li>`;
         }
+
         document.querySelector('.answer').innerHTML = answer;
     }
 
-    document.onclick = function (event) {
-        event.stopPropagation();
-        if (event.target.classList.contains('answer-variant') && step < quiz.length) {
-            //Если ли ключ (Тони Старка) в массива
-            if (result[event.target.dataset.v] != undefined) {
-                result[event.target.dataset.v]++;
-            } else {
-                result[event.target.dataset.v] = 1;
-            }
-            step++; //после того как проголосовал - надо нарастить шаг
+    // disabled = false для кнопки "Далее" после выбора любого input
+    document.querySelector('.answer').addEventListener("change", function (event) {
+        if (event.target.matches('.answer-variant') && step < quiz.length) {
+            document.querySelector('.btn-next').disabled = false;
+        }
+    })
 
-            // проверяем, есть ли еще вопросы. Если step последний - то вывод результатов
+    // События клике на "Далее"
+    document.querySelector('.btn-next').addEventListener("click", function(event) {
+        if (step < quiz.length) {
+            let checkedInput = Array.from(document.querySelectorAll('input:checked'));
+
+            if (result[checkedInput[0].value] != undefined) {
+                result[checkedInput[0].value]++;
+            } else {
+                result[checkedInput[0].value] = 1;
+            };
+
+            step++; //после того как проголосовали - наращиваем шаг
+
+            // проверяем, если step последний - то переход к выводу результатов
             if (step == quiz.length) {
                 document.querySelector('.question').remove();
                 document.querySelector('.answer').remove();
                 document.querySelector('h1').remove();
+                document.querySelector('.btn-next').remove();
                 showResult();
             } else {
                 showQuestion(step);
             }
         }
-        console.log(result);
-        showQuestion(step);
-    }
 
-    // ищем ключ с максимальным значением для вывода результата
+        document.querySelector('.btn-next').disabled = true;
+        // console.log(result);
+        showQuestion(step);
+    })
+
+    // Ищем ключ с максимальным значением и выводим результат
     function showResult() {
         let key = Object.keys(result).reduce(function(a,b) {
             return result[a] > result[b] ? a : b;
         });
-        //вывод результатов ответов, зная max ключ
+
+        //вывод результата ответов, зная max ключ
         let div = document.createElement('div');
         div.classList.add('result');
         div.innerHTML = answers[key]['description'];
         document.querySelector('main').appendChild(div);
 
-        //выводим изображение
+        //вывод изображения
         let divImg = document.createElement('div');
         divImg.classList.add('result-img');
         divImg.innerHTML = `
             <img src="img/${answers[key]['image']}" alt="">
         `;
-        // divImg.style.cssText = `
-        //         width: 500px;
-        //         height: 300px;
-        // `
         document.querySelector('main').appendChild(divImg);
     }
-
     showQuestion(step);
 }
